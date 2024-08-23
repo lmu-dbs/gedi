32b5982e34dd43968db19ebd5e191e63d59e3b09
anirudh <anirudhsai027@gmail.com>
2024-08-23 13:00:52 +0200

    bash debug 3/?

--- utils/config_fabric.py
+++ utils/config_fabric.py
@@ -359,9 +359,9 @@ if __name__ == '__main__':
             #     progress_bar.progress(i + 1)
 
             # Run the actual command
-            # result = subprocess.run(command, capture_output=True, text=True)
-            result = subprocess.run(['python', '-W', 'ignore', '-c', 'print("Hello, World!")'], capture_output=True, text=True)
-            st.write("bash results:",result.stdout)
+            result = subprocess.run(command, capture_output=True, text=True)
+            # result = subprocess.run(['python', '-W', 'ignore', '-c', 'print("Hello, World!")'], capture_output=True, text=True)
+            st.write("bash results:",result.stdout, result.stderr)
             st.write("## Results")
 
             # Collect all file paths from the output directory
