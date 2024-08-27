7ed2c067a64b4e1a5fe634fef4f3fbd9c20d41f0
anirudh <anirudhsai027@gmail.com>
2024-08-27 14:31:17 +0200

    minor changes to ui

--- utils/config_fabric.py
+++ utils/config_fabric.py
@@ -336,7 +336,7 @@ if __name__ == '__main__':
             st.code(f"python -W ignore main.py -a {output_path}", language='bash')
     if pipeline_steps == ["event_logs_generation"]:
         with button_col2:
-            create_run_button = st.button("Run config_file")
+            create_run_button = st.button("Run Generation")
     
         if create_run_button:
             # Save configuration to the specified output path
