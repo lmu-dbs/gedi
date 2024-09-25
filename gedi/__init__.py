1d9ed09baba6a0d435f4a3dfbc3eee3f00237d6f
Andrea Maldonado <>
2024-09-25 15:13:56 +0200

    Adds gedi module for pip installation

--- gedi/__init__.py
+++ gedi/__init__.py
@@ -3,5 +3,6 @@ from .features import EventLogFeatures
 from .augmentation import InstanceAugmentator
 from .benchmark import BenchmarkTest
 from .plotter import BenchmarkPlotter, FeaturesPlotter, AugmentationPlotter, GenerationPlotter
+from .run import gedi
 
-__all__=[ 'GenerateEventLogs', 'EventLogFeatures', 'FeatureAnalyser', 'InstanceAugmentator', 'BenchmarkTest', 'BenchmarkPlotter', 'FeaturesPlotter', 'AugmentationPlotter', 'GenerationPlotter']
+__all__=[ 'gedi', 'GenerateEventLogs', 'EventLogFeatures', 'FeatureAnalyser', 'InstanceAugmentator', 'BenchmarkTest', 'BenchmarkPlotter', 'FeaturesPlotter', 'AugmentationPlotter', 'GenerationPlotter']
