ae8f2b34098b2726f1f28ff4f2a1ba41fd1e1c17
Andrea Maldonado <>
2024-10-14 02:21:09 +0200

    Merge from main

--- gedi/__init__.py
+++ gedi/__init__.py
@@ -1,7 +1,3 @@
-from .generator import GenerateEventLogs
-from .features import EventLogFeatures
-from .augmentation import InstanceAugmentator
-from .benchmark import BenchmarkTest
-from .plotter import BenchmarkPlotter, FeaturesPlotter, AugmentationPlotter, GenerationPlotter
+from .run import gedi
 
-__all__=[ 'GenerateEventLogs', 'EventLogFeatures', 'FeatureAnalyser', 'InstanceAugmentator', 'BenchmarkTest', 'BenchmarkPlotter', 'FeaturesPlotter', 'AugmentationPlotter', 'GenerationPlotter']
+__all__=['gedi']
