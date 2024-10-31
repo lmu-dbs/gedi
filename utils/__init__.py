1e929eed188dacddd3a3839ede45fe83261053e1
Andrea Maldonado <>
2024-10-31 15:26:14 +0100

    Solves utils imports

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,5 @@
 from .param_keys.generator import GENERATOR_PARAMS, EXPERIMENT, CONFIG_SPACE, N_TRIALS
+from .param_keys import PIPELINE_STEP, INPUT_PATH, OUTPUT_PATH
 
 def function_name(function: callable):
     return str(function).split()[1]
