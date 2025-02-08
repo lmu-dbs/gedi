3c63359756390e706931a066b95aefbc809d6c5b
Andrea Maldonado <>
2025-02-08 20:42:26 +0100

    Updates to version 1.0.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-version_string = os.environ.get("VERSION_PLACEHOLDER", "1.0.5")
+version_string = os.environ.get("VERSION_PLACEHOLDER", "1.0.6")
 print(version_string)
 version = version_string
 
