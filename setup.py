a8f3da0cf9b44bb23d1e8e0b1df28c3dc3f80065
Andrea Maldonado <>
2025-02-08 20:53:52 +0100

    Updates version and debuggs deprecated artifact

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-version_string = os.environ.get("VERSION_PLACEHOLDER", "1.0.6")
+version_string = os.environ.get("VERSION_PLACEHOLDER", "1.0.7")
 print(version_string)
 version = version_string
 
